d75de1f8616e29d2514959c66c89febb16f3fb89
Andrew Noorishad <anoorishad@me.com>
2022-11-06 23:59:09 -0800

    Add files via upload

---
+++ M5-Challenge-Noorishad-Andrew/gamestore-serviceregistry/src/test/java/com/example/gamestoreserviceregistry/GamestoreServiceregistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gamestoreserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamestoreServiceregistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
