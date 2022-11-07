5e9fed466b5c1764f65f521d5565f5e3c49e3feb
Andrew Noorishad <anoorishad@me.com>
2022-11-06 23:57:50 -0800

    Delete M5-Challenge-Noorishad-Andrew/gamestore-serviceregistry directory

--- M5-Challenge-Noorishad-Andrew/gamestore-serviceregistry/gamestore-invoicing/src/main/java/com/example/gamestoreinvoicing/repository/ProcessingFeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.gamestoreinvoicing.repository;
-
-import com.example.gamestoreinvoicing.model.ProcessingFee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProcessingFeeRepository extends JpaRepository<ProcessingFee, String> {
-}
