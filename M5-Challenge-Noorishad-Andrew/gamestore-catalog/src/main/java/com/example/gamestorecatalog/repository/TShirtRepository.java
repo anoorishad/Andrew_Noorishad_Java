10ed3163f2338336b162a5f247bda839148dc8ab
Andrew Noorishad <anoorishad@me.com>
2022-11-07 00:00:30 -0800

    Add files via upload

---
+++ M5-Challenge-Noorishad-Andrew/gamestore-catalog/src/main/java/com/example/gamestorecatalog/repository/TShirtRepository.java
@@ -0,0 +1,13 @@
+package com.example.gamestorecatalog.repository;
+
+import com.example.gamestorecatalog.model.TShirt;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TShirtRepository extends JpaRepository<TShirt, Long> {
+    List<TShirt> findAllByColor(String color);
+    List<TShirt> findAllBySize(String size);
+}
